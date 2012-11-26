ab3aa9344b80f7b41072f86bae058b38685ea6c1
Matthew Epler <matthewepler@gmail.com>
2012-11-26 11:24:45 -0500

    debug false

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ def slugify(text, delim=u'-'):
 
 #--------------------------------------------------------- SERVER START-UP >>>
 if __name__ == "__main__":
-	app.debug = True
+	app.debug = False
 	
 	port = int(os.environ.get('PORT', 5000)) # locally PORT 5000, Heroku will assign its own port
 	app.run(host='0.0.0.0', port=port)
